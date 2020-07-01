5b2054e921830f60a2eacfe9076a2e9cbf73f1fd
WEN Hao <wenh06@gmail.com>
2020-07-02 01:01:27 +0800

    remove utils, preparing for submodule

--- database_reader/utils/utils_audio/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-docstring, to write
-"""
-
-from .voice import *
-
-
-__all__ = [s for s in dir() if not s.startswith('_')]
--- database_reader/utils/utils_misc/utils_crawler.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-utilities for crawling data
-"""
