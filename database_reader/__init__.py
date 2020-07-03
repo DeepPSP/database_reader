21fe14061ee0ce20354f1941f6534381d19175ac
WEN Hao <wenh06@gmail.com>
2020-07-03 09:40:14 +0800

    resolve relative import error

--- database_reader/__init__.py
+++ database_reader/__init__.py
@@ -17,17 +17,17 @@ _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 _PARENT_DIR = os.path.dirname(_BASE_DIR)
 _IN_SYS_PATH = [p for p in [_BASE_DIR, _PARENT_DIR] if p in sys.path]
 if len(_IN_SYS_PATH) == 0:
-    sys.path.append(_PARENT_DIR)
+    sys.path.append(_BASE_DIR)
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-from .base import *
-from .physionet_databases import *
-from .nsrr_databases import *
-from .audio_databases import *
-from .image_databases import *
-from .other_databases import *
+# from .base import *
+# from .physionet_databases import *
+# from .nsrr_databases import *
+# from .audio_databases import *
+# from .image_databases import *
+# from .other_databases import *
 
 
-__all__ = [s for s in dir() if not s.startswith('_')]
+# __all__ = [s for s in dir() if not s.startswith('_')]
