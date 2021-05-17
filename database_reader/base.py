95dcac09e09a68d51be622663853ad12abbbc859
WEN Hao <wenh06@gmail.com>
2021-05-17 16:56:22 +0800

    update base

--- database_reader/base.py
+++ database_reader/base.py
@@ -31,6 +31,7 @@ from .utils.common import *
 
 
 __all__ = [
+    "WFDB_Beat_Annotations", "WFDB_Non_Beat_Annotations", "WFDB_Rhythm_Annotations",
     "PhysioNetDataBase",
     "NSRRDataBase",
     "ImageDataBase",
@@ -38,6 +39,72 @@ __all__ = [
     "OtherDataBase",
     "ECGWaveForm",
 ]
+
+
+WFDB_Beat_Annotations = {
+    "N": "Normal beat",
+    "L": "Left bundle branch block beat",
+    "R": "Right bundle branch block beat",
+    "B": "Bundle branch block beat (unspecified)",
+    "A": "Atrial premature beat",
+    "a": "Aberrated atrial premature beat",
+    "J": "Nodal (junctional) premature beat",
+    "S": "Supraventricular premature or ectopic beat (atrial or nodal)",
+    "V": "Premature ventricular contraction",
+    "r": "R-on-T premature ventricular contraction",
+    "F": "Fusion of ventricular and normal beat",
+    "e": "Atrial escape beat",
+    "j": "Nodal (junctional) escape beat",
+    "n": "Supraventricular escape beat (atrial or nodal)",
+    "E": "Ventricular escape beat",
+    "/": "Paced beat",
+    "f": "Fusion of paced and normal beat",
+    "Q": "Unclassifiable beat",
+    "?": "Beat not classified during learning",
+}
+
+WFDB_Non_Beat_Annotations = {
+    "[": "Start of ventricular flutter/fibrillation",
+    "!": "Ventricular flutter wave",
+    "]": "End of ventricular flutter/fibrillation",
+    "x": "Non-conducted P-wave (blocked APC)",
+    "(": "Waveform onset",
+    ")": "Waveform end",
+    "p": "Peak of P-wave",
+    "t": "Peak of T-wave",
+    "u": "Peak of U-wave",
+    "`": "PQ junction",
+    "'": "J-point",
+    "^": "(Non-captured) pacemaker artifact",
+    "|": "Isolated QRS-like artifact",
+    "~": "Change in signal quality",
+    "+": "Rhythm change",
+    "s": "ST segment change",
+    "T": "T-wave change",
+    "*": "Systole",
+    "D": "Diastole",
+    "=": "Measurement annotation",
+    '"': "Comment annotation",
+    "@": "Link to external data",
+}
+
+WFDB_Rhythm_Annotations = {
+    "(AB": "Atrial bigeminy",
+    "(AFIB": "Atrial fibrillation",
+    "(AFL": "Atrial flutter",
+    "(B": "Ventricular bigeminy",
+    "(BII": "2° heart block",
+    "(IVR": "Idioventricular rhythm",
+    "(N": "Normal sinus rhythm",
+    "(NOD": "Nodal (A-V junctional) rhythm",
+    "(P": "Paced rhythm",
+    "(PREX": "Pre-excitation (WPW)",
+    "(SBR": "Sinus bradycardia",
+    "(SVTA": "Supraventricular tachyarrhythmia",
+    "(T": "Ventricular trigeminy",
+    "(VFL": "Ventricular flutter",
+    "(VT": "Ventricular tachycardia",
+}
 
 
 class _DataBase(object):
@@ -491,75 +558,14 @@ class PhysioNetDataBase(_DataBase):
         attrs = vars(self)
         methods = [func for func in dir(self) if callable(getattr(self, func)) and not (func.startswith("__") and func.endswith("__"))]
 
-        beat_annotations = {
-            "N": "Normal beat",
-            "L": "Left bundle branch block beat",
-            "R": "Right bundle branch block beat",
-            "B": "Bundle branch block beat (unspecified)",
-            "A": "Atrial premature beat",
-            "a": "Aberrated atrial premature beat",
-            "J": "Nodal (junctional) premature beat",
-            "S": "Supraventricular premature or ectopic beat (atrial or nodal)",
-            "V": "Premature ventricular contraction",
-            "r": "R-on-T premature ventricular contraction",
-            "F": "Fusion of ventricular and normal beat",
-            "e": "Atrial escape beat",
-            "j": "Nodal (junctional) escape beat",
-            "n": "Supraventricular escape beat (atrial or nodal)",
-            "E": "Ventricular escape beat",
-            "/": "Paced beat",
-            "f": "Fusion of paced and normal beat",
-            "Q": "Unclassifiable beat",
-            "?": "Beat not classified during learning",
-        }
-
-        non_beat_annotations = {
-            "[": "Start of ventricular flutter/fibrillation",
-            "!": "Ventricular flutter wave",
-            "]": "End of ventricular flutter/fibrillation",
-            "x": "Non-conducted P-wave (blocked APC)",
-            "(": "Waveform onset",
-            ")": "Waveform end",
-            "p": "Peak of P-wave",
-            "t": "Peak of T-wave",
-            "u": "Peak of U-wave",
-            "`": "PQ junction",
-            "'": "J-point",
-            "^": "(Non-captured) pacemaker artifact",
-            "|": "Isolated QRS-like artifact",
-            "~": "Change in signal quality",
-            "+": "Rhythm change",
-            "s": "ST segment change",
-            "T": "T-wave change",
-            "*": "Systole",
-            "D": "Diastole",
-            "=": "Measurement annotation",
-            '"': "Comment annotation",
-            "@": "Link to external data",
-        }
-
-        rhythm_annotations = {
-            "(AB": "Atrial bigeminy",
-            "(AFIB": "Atrial fibrillation",
-            "(AFL": "Atrial flutter",
-            "(B": "Ventricular bigeminy",
-            "(BII": "2° heart block",
-            "(IVR": "Idioventricular rhythm",
-            "(N": "Normal sinus rhythm",
-            "(NOD": "Nodal (A-V junctional) rhythm",
-            "(P": "Paced rhythm",
-            "(PREX": "Pre-excitation (WPW)",
-            "(SBR": "Sinus bradycardia",
-            "(SVTA": "Supraventricular tachyarrhythmia",
-            "(T": "Ventricular trigeminy",
-            "(VFL": "Ventricular flutter",
-            "(VT": "Ventricular tachycardia",
-        }
+        beat_annotations = deepcopy(WFDB_Beat_Annotations)
+        non_beat_annotations = deepcopy(WFDB_Non_Beat_Annotations)
+        rhythm_annotations = deepcopy(WFDB_Rhythm_Annotations)
 
         all_annotations = [
             beat_annotations,
             non_beat_annotations,
-            rhythm_annotations
+            rhythm_annotations,
         ]
 
         summary_items = [
